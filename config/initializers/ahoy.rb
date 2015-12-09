2f3e11a4923b4333caf0f907c8f88c217fa06c08
Dietrich <dietricherwane@yahoo.fr>
2015-12-09 18:49:05 +0000

    changes

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,12 +1,12 @@
 class Ahoy::Store < Ahoy::Stores::ActiveRecordStore
   # customize here
-  def exclude?
-    bot? || request.ip == "192.168.1.1"
-  end
+  #def exclude?
+    #bot? || request.ip == "192.168.1.1"
+  #end
 
-  Ahoy.cookie_domain = :all
+  #Ahoy.cookie_domain = :all
 
-  Ahoy.visit_duration = 30.minutes
+  #Ahoy.visit_duration = 30.minutes
 
-  Ahoy.track_visits_immediately = true
+  #Ahoy.track_visits_immediately = true
 end
