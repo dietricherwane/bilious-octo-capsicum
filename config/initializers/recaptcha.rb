44bbeb1cceb2f9674f4ab6f54c9c68ee0f445e7f
Dietrich <dietricherwane@yahoo.fr>
2016-03-18 08:40:30 +0000

    changes

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,6 @@
+Recaptcha.configure do |config|
+  config.public_key  = '6LdmDxsTAAAAAEMGhuzDifWoAocciBis_xdUpb7E'
+  config.private_key = '6LdmDxsTAAAAAECPP8o7YpEQKYgrFybjpcMsBy6n'
+  # Uncomment the following line if you are using a proxy server:
+  # config.proxy = 'http://myproxy.com.au:8080'
+end
